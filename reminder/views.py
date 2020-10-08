b07f1118b6a533b36ed76e4f00a1f4c977d87a7b
favianjg <rafcath95@gmail.com>
2020-10-08 21:20:24 +0200

    Added perform create and update to catch post and put method during user interaction. Built the email messaging app to respond to post and put request methods

--- reminder/views.py
+++ reminder/views.py
@@ -1,6 +1,10 @@
+from datetime import datetime, timedelta
+
+from pytz import timezone
 from rest_framework import viewsets
 
 from reminder.models import Reminders
+from reminder.sendmail_task import send_mail_task
 from reminder.serializers import ReminderSerializer
 
 
@@ -12,3 +16,35 @@ class ReminderViewSet(viewsets.ModelViewSet):
 
     queryset = Reminders.objects.all()
     serializer_class = ReminderSerializer
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            validatedData = serializer.validated_data
+            email = validatedData.get('email')
+            subject = "Reminder from DRFRocketLoop"
+            text = validatedData.get('text')
+            seconds = validatedData.get('delay')
+
+            my_tz = timezone('Europe/Berlin')
+
+            send_mail_task.apply_async(
+                ([email], subject, text),
+                eta=(my_tz.localize(datetime.now())
+                     + timedelta(seconds=seconds * 60)))
+            serializer.save()
+
+    def perform_update(self, serializer):
+        if serializer.is_valid():
+            validatedData = serializer.validated_data
+            email = validatedData.get('email')
+            subject = "Reminder from DRFRocketLoop"
+            text = validatedData.get('text')
+            seconds = validatedData.get('delay')
+
+            my_tz = timezone('Europe/Berlin')
+
+            send_mail_task.apply_async(
+                ([email], subject, text),
+                eta=(my_tz.localize(datetime.now())
+                     + timedelta(seconds=seconds * 60)))
+            serializer.save()
