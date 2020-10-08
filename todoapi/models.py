a2eed6bd452d86a3fd5424d6b58ddc99ed5506c3
favianjg <rafcath95@gmail.com>
2020-10-08 19:19:26 +0200

    Initial commit. Initial model for database migration

---
+++ todoapi/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+from pygments.lexers import get_all_lexers
+
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+
+
+class Boards(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='nonameboard')
+
+    class Meta:
+        ordering = ['name']
+
+class Todos(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='nonametodo')
+    done = models.BooleanField(default=False)
+    board = models.ForeignKey(Boards, related_name='todos', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['title']
