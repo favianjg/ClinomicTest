1c97706bc46a0968970bb33a3ac7a6f6deeedd08
favianjg <rafcath95@gmail.com>
2020-10-08 21:06:36 +0200

    Added an email messaging script using celery

---
+++ reminder/sendmail_task.py
@@ -0,0 +1,19 @@
+from celery.utils.log import get_task_logger
+from django.conf import settings
+from django.core.mail import send_mail
+
+from ClinomicTest.celery import app
+
+
+@app.task
+def send_mail_task(recipients, subject, context):
+    logger = get_task_logger(__name__)
+    send_mail(
+        subject=subject,
+        message=context,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=recipients,
+        fail_silently=False,
+        html_message=context
+    )
+    logger.info('Email sent')
