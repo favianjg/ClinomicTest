4f119bc92a2281d050fc6db2fb28593d0f0b82da
favianjg <rafcath95@gmail.com>
2020-10-08 20:13:11 +0200

    Edited urls for both todoapi and project folders. Ready for initial testing

--- ClinomicTest/urls.py
+++ ClinomicTest/urls.py
@@ -14,8 +14,37 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.contrib.auth.models import User
+from django.urls import path, include
+from rest_framework import routers, serializers, viewsets
+
+from todoapi.views import BoardViewSet, TodoViewSet, TodoNotDoneViewSet, BoardDetailViewSet
+
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'board_overview', BoardViewSet)
+router.register(r'board_detail', BoardDetailViewSet)
+router.register(r'todo_list', TodoViewSet)
+router.register(r'todo_notdone', TodoNotDoneViewSet, basename='Todos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('', include('todoapi.urls')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
---
+++ todoapi/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+
+from todoapi import views
+
+board_view = views.BoardViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+board_detail = views.BoardDetailViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+todo_list = views.TodoViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+
+todo_detail = views.TodoViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+todo_notdone = views.TodoNotDoneViewSet.as_view({
+    'get': 'list'
+})
+
+urlpatterns = [
+    path('boards/', board_view, name='boardsoverview'),
+    path('boardsdetail/<int:pk>/', board_detail, name='boardsdetail'),
+    path('todos/', todo_list, name='todoslist'),
+    path('todos/<int:pk>/', todo_detail, name='todosdetail'),
+    path('todosnotdone/', todo_notdone, name='todosnotdone')
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
