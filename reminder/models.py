2d67ddc575f638c436f6382a5fc5b63df9e6dd2c
favianjg <rafcath95@gmail.com>
2020-10-08 20:38:04 +0200

    created the basic models for reminder

--- reminder/models.py
+++ reminder/models.py
@@ -1,1 +1,13 @@
-# Create your models here.
+from django.db import models
+from pygments.lexers import get_all_lexers
+
+LEXERS = [item for item in get_all_lexers() if item[1]]
+
+
+class Reminders(models.Model):
+    email = models.EmailField()
+    text = models.TextField()
+    delay = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['email']
